fa4e9a583656703b75fe4cfaffedb8ce4f58020f
Elly Oxy <ooxxyyr@protonmail.com>
2023-05-26 16:24:35 +0200

    reviewed

--- Lab_SQL_Intro.sql
+++ Lab_SQL_Intro.sql
@@ -10,15 +10,15 @@ select title
 from film;
 
 -- Get unique list of film languages under the alias language. Note that we are not asking you to obtain the language per each film, but this is a good time to think about how you might get that information in the future.
-select name
+select `name` as `language`
 from language;
 
 --  Find out how many stores does the company have?
-select store_id
+select count(store_id)
 from store;
 
 -- Find out how many employees staff does the company have?
-select staff_id
+select count(staff_id)
 from staff;
 
 -- Return a list of employee first names only?
